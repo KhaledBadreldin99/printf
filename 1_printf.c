517beefb863eb0a5160161e26fb0cf21f2732013
khloud54 <kh.t705042000@gmail.com>
2023-09-17 14:15:29 -0700

    1_printf.c

--- 1_printf.c
+++ 1_printf.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * _putInt - function that print an int
+ * print_integer - print a number in base 10
+ * @_putInt - function that print an int
  * @num: the integer we want to print
  * @pCount: pointer to the counter to know num of int being printed
+ * Return: Length of the numbers in decimal
 */
 void _putInt(unsigned int num, int *pCount)
 {
-	if ((int)num < 0)
-	{
-		_putchar('-');
-		*pCount += 1;
-		num *= -1;
-	}
+	char *p_buff;
+	int size;
 
-	if (num / 10)
-		_putInt(num / 10, pCount);
+	p_buff = itoa(va_arg(list, int), 10);
 
-	_putchar(num % 10 + '0');
-	*pCount += 1;
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
 }
