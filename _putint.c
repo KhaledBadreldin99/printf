77f67ec3dd35c571d1f8b25a361a2f5fa04f7034
KhaledBadreldin99 <khaled.badr24.kb@gmail.com>
2023-09-17 14:28:28 -0700

    _putint.c

---
+++ _putint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _putInt - function print int
+ * @num: int to print
+ * @pCount: pointer to the counter
+ */
+void _putInt(unsigned int num, int *pCount)
+{
+	/* handle -ve numbers*/
+	if ((int)num < 0)
+	{
+		_putchar('-');
+		*pCount += 1;
+		num *= -1;
+	}
+	/* number more than 1 digit*/
+	if (num / 10)
+	{
+		putInt(num / 10, pCount);
+	}
+	_putchar(num % 10 + '0');
+	*pCount += 1;
+}
